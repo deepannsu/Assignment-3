0c4c8a32c546d3384865618709a8b5374f1ba1f5
deepannsu <122818510+deepannsu@users.noreply.github.com>
2023-10-01 23:25:29 -0400

    write the code for $.each()

--- WEB303As03-01/WEB303As03/js/script.js
+++ WEB303As03-01/WEB303As03/js/script.js
@@ -14,6 +14,27 @@ function getTeamDataWithGetJSON() {
   });
 }
 
+// Method for $.ajax request
+function getTeamDataWithAjax() {
+  $.ajax({
+    url: "team.json",
+    dataType: "json",
+    success: function(data) {
+      // Loop through the array of team members
+      $.each(data.team, function(index, member) {
+        // Create HTML elements for name, position, and bio
+        var nameElement = $("<h2>").text(member.name);
+        var positionElement = $("<h5>").text(member.position);
+        var bioElement = $("<p>").text(member.bio);
 
+        // Append the elements to the #team div
+        $("#team").append(nameElement, positionElement, bioElement);
+      });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.error("Error loading team data: " + textStatus, errorThrown);
+    }
+  });
+}
 
   
