711ce3692002374779abf2f4a68749c60b0b5d92
deepannsu <122818510+deepannsu@users.noreply.github.com>
2023-10-01 23:22:54 -0400

    write the code for $.getjson method

--- WEB303As03-01/WEB303As03/js/script.js
+++ WEB303As03-01/WEB303As03/js/script.js
@@ -1,71 +1,19 @@
 // Method for $.getJSON request
 function getTeamDataWithGetJSON() {
-    $.getJSON("team.json", function(data) {
-      // Loop through the array of team members
-      $.each(data.team, function(index, member) {
-        // Create HTML elements for name, position, and bio
-        var nameElement = $("<h2>").text(member.name);
-        var positionElement = $("<h5>").text(member.position);
-        var bioElement = $("<p>").text(member.bio);
-  
-        // Append the elements to the #team div
-        $("#team").append(nameElement, positionElement, bioElement);
-      });
-    });
-  }
-  
-  // Method for $.ajax request
-  function getTeamDataWithAjax() {
-    $.ajax({
-      url: "team.json",
-      dataType: "json",
-      success: function(data) {
-        // Loop through the array of team members
-        $.each(data.team, function(index, member) {
-          // Create HTML elements for name, position, and bio
-          var nameElement = $("<h2>").text(member.name);
-          var positionElement = $("<h5>").text(member.position);
-          var bioElement = $("<p>").text(member.bio);
-  
-          // Append the elements to the #team div
-          $("#team").append(nameElement, positionElement, bioElement);
-        });
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.error("Error loading team data: " + textStatus, errorThrown);
-      }
-    });
-  }
+  $.getJSON("team.json", function(data) {
+    // Loop through the array of team members
+    $.each(data.team, function(index, member) {
+      // Create HTML elements for name, position, and bio
+      var nameElement = $("<h2>").text(member.name);
+      var positionElement = $("<h5>").text(member.position);
+      var bioElement = $("<p>").text(member.bio);
 
-  // Method for $.ajax request
-function getTeamDataWithAjax() {
-    // Display "Loading..." message in the #team div
-    $("#team").text("Loading...");
-  
-    $.ajax({
-      url: "team.json",
-      type: "GET",
-      dataType: "json",
-      success: function(data) {
-        // Clear the "Loading..." message
-        $("#team").empty();
-  
-        // Loop through the array of team members
-        $.each(data.team, function(index, member) {
-          // Create HTML elements for name, position, and bio
-          var nameElement = $("<h2>").text(member.name);
-          var positionElement = $("<h5>").text(member.position);
-          var bioElement = $("<p>").text(member.bio);
-  
-          // Append the elements to the #team div
-          $("#team").append(nameElement, positionElement, bioElement);
-        });
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // Display an error message in the #team div
-        $("#team").text("Error: Could not retrieve content.");
-        console.error("Error loading team data: " + textStatus, errorThrown);
-      }
+      // Append the elements to the #team div
+      $("#team").append(nameElement, positionElement, bioElement);
     });
-  }
+  });
+}
+
+
+
   
